2accc38bb5cf19ca03e870de66d74a8f714866f7
Matthew Woodruff <mwoodruf@thoughtworks.com>
2021-02-22 23:45:14 +0000

    Reformat code in retriever.ts

--- src/dependency-tree/retriever.ts
+++ src/dependency-tree/retriever.ts
@@ -8,13 +8,13 @@ import {PackageRetriever} from "./port/packageRetriever.types";
 
 type PackageRetrieverBuilder = (pr: PackageRetriever) => PackageRetriever;
 
-const buildPackageRetriever = (packageRetriever: PackageRetriever, sessionPackageRetriever?: PackageRetrieverBuilder): PackageRetriever =>
-    sessionPackageRetriever ? sessionPackageRetriever(packageRetriever) : packageRetriever
-
 export const buildDependencyTreeRetriever =
     (versionResolver: VersionResolver, packageRetriever: PackageRetriever, sessionPackageRetriever?: PackageRetrieverBuilder) =>
         (npmPackageRequest: NpmPackageRequestCommand) =>
             getDependencyTree(npmPackageRequest, versionResolver, buildPackageRetriever(packageRetriever, sessionPackageRetriever));
+
+const buildPackageRetriever = (packageRetriever: PackageRetriever, sessionPackageRetriever?: PackageRetrieverBuilder): PackageRetriever =>
+    sessionPackageRetriever ? sessionPackageRetriever(packageRetriever) : packageRetriever
 
 const getDependencyTree = async (
     {name, version}: NpmPackageRequestCommand,
